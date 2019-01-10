c462ce23405bd0c20f2449b75e21c50c54ace069
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2019-01-09 21:23:27 -0500

    changed to singleton design pattern

--- app/src/main/java/com/example/max/todo/ToDoManager.java
+++ app/src/main/java/com/example/max/todo/ToDoManager.java
@@ -6,20 +6,35 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-public class TodoManager implements Serializable {
-    // id of this TodoManager
+public class ToDoManager implements Serializable {
+    // id of this ToDoManager
     private UUID id;
 
     // collection of ToDos
     private List<ToDoTask> todos;
 
-    public TodoManager() {
+    // instance
+    private static final ToDoManager instance = new ToDoManager();
+
+    private ToDoManager() {
         this.id = UUID.randomUUID();
         this.todos = new ArrayList<>();
+    }
+
+    public static ToDoManager getInstance() {
+        return instance;
     }
 
     public void addToDo(ToDoTask t) {
         this.todos.add(t);
         Collections.sort(todos);
+    }
+
+    public ToDoTask getTask(int pos) {
+        return todos.get(pos);
+    }
+
+    public int getNumberOfTasks() {
+        return todos.size();
     }
 }

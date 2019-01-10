b72b8189dd2de7d1f249824b7f6402a1ec6f2dc3
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2019-01-09 21:20:30 -0500

    Created main activity with recycler view.

--- app/src/main/java/com/example/max/todo/MainActivity.java
+++ app/src/main/java/com/example/max/todo/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.max.todo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private ToDoManager manager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.manager = ToDoManager.getInstance();
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerViewAdapter(this.manager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.plusmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.plusbutton) {
+            Intent intent = new Intent(this, CreateToDoActivity.class);
+            startActivity(intent);
+        }
+//        ToDoTask test = new ToDoTask(LocalDateTime.from(LocalDateTime.now()),
+//                LocalDateTime.from(LocalDateTime.now()), "Dog", "pooooooop");
+//        this.manager.addToDo(test);
+//        mAdapter.notifyDataSetChanged();
+        return true;
     }
 }

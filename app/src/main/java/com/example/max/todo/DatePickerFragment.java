0a60717d2414920c0c9267e5d4014ca052baa059
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2019-01-11 02:26:45 -0500

    Created datefragment to set date.

---
+++ app/src/main/java/com/example/max/todo/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package com.example.max.todo;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity());
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}

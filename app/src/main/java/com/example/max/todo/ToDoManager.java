cc60628810192ae922bb1a596eca82d00e509344
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2019-01-20 16:13:46 -0500

    Added comments

--- app/src/main/java/com/example/max/todo/ToDoManager.java
+++ app/src/main/java/com/example/max/todo/ToDoManager.java
@@ -7,33 +7,63 @@ import java.util.List;
 import java.util.UUID;
 
 public class ToDoManager implements Serializable {
-    // id of this ToDoManager
+    /**
+     * Unique ID of ToDoManager
+     */
     private UUID id;
 
-    // collection of ToDos
+    /**
+     * Collection of ToDoTasks
+     */
     private List<ToDoTask> todos;
 
-    // instance
+    /**
+     * instance of ToDoManager
+     */
     private static final ToDoManager instance = new ToDoManager();
 
+    /**
+     * Constructor to create first ToDoManager
+     */
     private ToDoManager() {
         this.id = UUID.randomUUID();
         this.todos = new ArrayList<>();
     }
 
+    /**
+     * Return the unique ToDoManager instance
+     *
+     * @return ToDoManager instance
+     */
     public static ToDoManager getInstance() {
         return instance;
     }
 
+    /**
+     * Add new ToDoTask t into the list of ToDos stored and sort.
+     *
+     * @param t ToDoTask
+     */
     public void addToDo(ToDoTask t) {
         this.todos.add(t);
         Collections.sort(this.todos);
     }
 
+    /**
+     * Return the ToDoTask at the specified index pos.
+     *
+     * @param pos index of the ToDoTask
+     * @return ToDoTask at pos
+     */
     public ToDoTask getTask(int pos) {
         return todos.get(pos);
     }
 
+    /**
+     * Return the number of items in the todos list
+     *
+     * @return number of ToDoTasks
+     */
     public int getNumberOfTasks() {
         return todos.size();
     }

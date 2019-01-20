2cd7c3ab682fd7dc2b46812cd92ece7e742f41d7
Maxwell Garrett <maxwell.garrett@mail.utoronto.ca>
2019-01-20 04:08:05 -0500

    Moved Listener into CreateToDoActivity

--- app/src/main/java/com/example/max/todo/DatePickerFragment.java
+++ app/src/main/java/com/example/max/todo/DatePickerFragment.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.DatePicker;
 
-public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new DatePickerDialog(getActivity());
-    }
+
+
+public class DatePickerFragment extends DialogFragment {
 
     @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        ((CreateToDoActivity) getActivity()).editDueTime(year, month, dayOfMonth);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        String[] date = getArguments().getString("date").split("-");
+        int dayOfMonth = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+        return new DatePickerDialog(getActivity(), (CreateToDoActivity) getActivity(), year, month, dayOfMonth);
     }
+
+
 }
